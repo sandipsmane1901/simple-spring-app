73ef3de44f8e18e1f1075d005c1891ab19b3abb5
sandipsmane <sandipsmane@gmail.com>
2026-05-09 21:22:25 +0530

    add code files

---
+++ src/test/java/com/example/simple_spring_app/SimpleSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
